e4d67b5e614552ed17c62f9070a4c97f287f28f1
amogh4319 <amogh4319@gmail.com>
2023-08-11 20:33:14 +0530

    protected product page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React from "react";
+import React, { useContext } from "react";
 //import { Container } from "react-bootstrap";
 
 import Navigation from "./components/Navigation"
@@ -7,15 +7,16 @@ import ProductProvider from "./store/ProductProvider";//Make sure to create this
 import AboutUs from "./components/AboutUs";
 import HomePage from "./components/HomePage";
 import ContactUs from "./components/ContactUs";
-import {Route,Routes} from 'react-router-dom'
+import {Route,Routes,Navigate} from 'react-router-dom'
 import ProductList from "./components/ProductList";
 import Products from "./components/Products";
 import ProductPage from "./components/ProductPage";
 import Login from "./components/Login";
+import AuthContext from "./store/auth-context";
 
 
 function App() {
- 
+ const ctx=useContext(AuthContext);
   return (
   
     <ProductProvider>
@@ -25,10 +26,11 @@ function App() {
           <Routes>
           <Route path='/' element={<Login/>}></Route>
           <Route path='/home' element={<HomePage/>}></Route>
-          <Route path='/store' element={<ProductList products={Products}/>}></Route>
+          {ctx.isLoggedIn&&<Route path='/store' element={<ProductList products={Products}/>}></Route>}
+          {!ctx.isLoggedIn&&<Route path='/store' element={<Navigate to='/' />}></Route>}
           <Route path='/about' element={<AboutUs/>}></Route>
           <Route path='/contact' element={<ContactUs/>}></Route>
-          <Route path='/store/:productId' element={<ProductPage/>}></Route>
+          {ctx.isLoggedIn&&<Route path='/store/:productId' element={<ProductPage/>}></Route>}
           </Routes>
         </main>
         
